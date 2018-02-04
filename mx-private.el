0fa090e981e6937f8b767e65e818eeac483eecee
re-mx <reimu-mx@outlook.com>
2018-02-04 12:21:46 +0800

    mx-private.el: More safely set company variables

--- mx-private.el
+++ mx-private.el
@@ -2,12 +2,13 @@
 
 
 (dolist (hook '(c-mode-hook c++-mode-hook))
-  (add-hook hook (lambda ()
-                   (setq-local company-minimum-prefix-length 2)
-                   (setq-local company-backends '((company-irony company-irony-c-headers)
-                                                  company-dabbrev-code
-                                                  company-files
-                                                  company-dabbrev)))))
+  (with-eval-after-load 'company
+    (add-hook hook (lambda ()
+                     (setq-local company-minimum-prefix-length 2)
+                     (setq-local company-backends '((company-irony company-irony-c-headers)
+                                                    company-dabbrev-code
+                                                    company-files
+                                                    company-dabbrev))))))
 
 (with-eval-after-load 'tern
   (add-to-list 'tern-command "--no-port-file" 'append))
