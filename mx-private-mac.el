8db770126e76c20fe120dcad6c7f24ac9d0c79a7
re-mx <reimu-mx@outlook.com>
2018-09-08 13:05:13 +0800

    mx-private-mac.el: Add transparency settings

--- mx-private-mac.el
+++ mx-private-mac.el
@@ -5,4 +5,8 @@
 (global-set-key (kbd "C-.") 'set-mark-command)
 (global-set-key (kbd "C-x C-.") 'pop-global-mark)
 
+(add-to-list 'default-frame-alist '(ns-transparent-titlebar . t))
+
+(remx/set-transparency 88)
+
 (provide 'mx-private)
