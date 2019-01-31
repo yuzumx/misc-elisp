4a838029938b067a31f1dfd5b76804cb3a2d77de
re-mx <reimu-mx@outlook.com>
2019-01-31 19:02:32 +0800

    mx-private.el: Enable flycheck-mode in elisp

--- mx-private.el
+++ mx-private.el
@@ -22,8 +22,6 @@
 (put 'upcase-region 'disabled nil)
 (put 'downcase-region 'disabled nil)
 
-(add-hook 'emacs-lisp-mode-hook (lambda () (flycheck-mode -1)))
-
 (global-set-key (kbd "M-y") 'counsel-yank-pop)
 
 (provide 'mx-private)
